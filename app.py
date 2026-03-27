93c713fcf88eaea4820ae9640ccf5ef481939418
THAKOR SHUBHAMKUMAR <thakorshubham548@gmail.com>
2026-03-28 00:37:26 +0530

    Add Streamlit background remover application
    Implemented a Streamlit app for background removal from images, allowing users to upload an image, remove its background, and download the processed image.

--- app.py
+++ app.py
@@ -1,7 +1,47 @@
-from player import notification  
+import streamlit as st 
+import os 
+from PIL import Image
+from rembg import remove
+import io
 
-notification.notify(
-    title = 'hello shubham',
-    message = 'lets play a game',
-    timeout = 10
+st.title("🖼️ Background Remover")
+
+uploded_file = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
+
+if uploded_file:
+    img = Image.open(uploded_file)
+
+    st.subheader("Original Image")
+    st.image(img, use_container_width=True)
+
+    if st.button("Remove Background"):
+        with st.spinner("Removing Background..."):
+            output = remove(img)
+
+        st.subheader("Background Removed Image")
+        st.image(output)
+
+        # Convert image to bytes for download
+        buf = io.BytesIO()
+        output.save(buf, format="PNG")
+        byte_im = buf.getvalue()
+
+        # Download button
+        st.download_button(
+            label="⬇️ Download Image",
+            data=byte_im,
+            file_name="background_removed.png",
+            mime="image/png"
+        )
+
+st.markdown("---")
+
+# Footer
+st.markdown(
+    """
+    ### 👨‍💻 Created by Mr Shubham
+    
+    🔗 GitHub: https://github.com/shubham0182  
+    📸 Instagram: https://instagram.com/_shubhhh_012
+    """
 )
